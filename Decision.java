d5f8ad9d9c7449390ab2d5fbf7837f6ec8015060
enmalopezbran <enmalopezbran@gmail.com>
2016-08-09 20:38:57 -0600

    Update Decision.java

--- Decision.java
+++ Decision.java
@@ -1,38 +1,72 @@
-public class Decision{
-	protected int degrees;
-	protected String decision;
-	protected boolean seguir;
+/**
+ *
+ * @author Enma Lopez
+ */
+public class Decision {
+        private boolean right;
+        private boolean left;
+	private boolean front;
+        private String mensaje;
+	
 
 	public Decision(){
-		degrees = 0;
-		orientation = "front"
-		seguir = true;
+	    right = false;
+            left = false;
+            front = false;
+    
 	}
 
-	public Decision(String des){
-		seguir = true;
-		switch (desicion){
-			case "takeARight":
-				degrees = 45;
-				decision = "right"
+	public Decision(String opcion){
+		switch (opcion){
+			case "XXX":
+				right = false;
+                                front = false;
+                                left = false;
+                                mensaje = "STOP";
+                                break;
+                               
+			case "XXO":
+				right= false;
+                                front = false;
+                                left = true;
+                                mensaje = "Girar izquierda";
 				break;
-			case "takeALeft":
-				degrees = 45;
-				decision = "left"
+                            
+			case "XOO":
+				right = false;
+                                front = true;
+                                left = true;
+                                mensaje = "Seguir Recto";
+                                break;
+				
+			case "OXX":
+			        right = true;
+                                front = true;
+                                left = true;
+                                mensaje = "Girar derecha";
+                                break;
+                            
+	                case "OXO":
+			        right= true;
+                                front = false;
+                                left = true;
+                                mensaje = "Seguir derecha";
+                                break;
+				
+			case "OOX":
+				right = true;
+				front = true;
+				left = false;
+                                mensaje = "Seguir recto";
 				break;
-			case "keepTrue":
-				degrees = 0;
-				decision = "front"
-				break;
-			case "turnBack":
-				degrees = 180;
-				decision = "back"
-				seguir = true;
-			case "stop":
-				degrees = 0;
-				decision = "front"
-				seguir = false;
-				break;
+                            
+                        case "OOO":
+                                right = true;
+				front = true;
+				left = true;
+                                mensaje = "Seguir recto";
+                                break;
+                            
 		}
 	}
 

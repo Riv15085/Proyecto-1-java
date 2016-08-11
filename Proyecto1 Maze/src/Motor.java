068560a0e6f856a64fcd74cecaea7a3fc9ac019d
jpcahueque <jpcahueque@gmail.com>
2016-08-10 19:08:07 -0600

    Update Motor.java

--- Proyecto1 Maze/src/Motor.java
+++ Proyecto1 Maze/src/Motor.java
@@ -1,14 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/*
+Universidad del Valle de Guatemala
+Proyecto1 AED
+Juan Pablo Cahueque
+Enma Lopez
+Diego Rivera
+Jorge Tezen
+Motor.java
+*/
 
 /**
  *
- * @author Usuario
+ * @author JuanPablo
  */
 public class Motor {
     
     boolean motorOn;
-    
+    //En la simulacion se imprime lo que haria el robot en la vida real
     public Motor(){
-        
+        motorOn = true;
     }
     
     public void Avanzar(int tiempo){
@@ -26,15 +40,16 @@ public class Motor {
     }
     
     public void moverN(String movimiento){
+        //Realiza el inverso de lo que habia echo anteriormente para regresar a un estado especifico
         switch(movimiento){
             case ("Seguir Recto"):
                 System.out.println("Mover atras 1");
                 break;
-            case ("Girar Derecha"):
-                System.out.println("Girar Izquierda");
+            case ("Girar Derecha y avanzar"):
+                System.out.println("Retoceder y girar Izquierda");
                 break;
-            case ("Girar Izquierda"):
-                System.out.println("Girar Derecha");
+            case ("Girar Izquierda y avanzar"):
+                System.out.println("Retroceder y girar Derecha");
                 break;
         }
     }
